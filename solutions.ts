c4f876dfbab3ce4c9571d0f12fba4544075a0a30
Asadullah <maazihadin@gmail.com>
2026-05-07 19:00:40 +0600

    Problem seven solved

--- solutions.ts
+++ solutions.ts
@@ -3,16 +3,16 @@ function filterEvenNumbers(numArr: number[]): number[] {
   return numArr.filter((num) => num % 2 === 0);
 }
 const numbers: number[] = [1, 2, 3, 4, 5, 6];
-const resultP1 = filterEvenNumbers(numbers);
-console.log(resultP1);
+const result1 = filterEvenNumbers(numbers);
+console.log(result1);
 
 //=========>>>Problem 2<<<===========//
 function reverseString(str: string): string {
   const revStr = str.split('').reverse().join('');
   return revStr;
 }
-const resultP2 = reverseString('typescript');
-console.log(resultP2);
+const result2 = reverseString('typescript');
+console.log(result2);
 
 //=========>>>Problem 3<<<===========//
 type StringOrNumber = string | number;
@@ -23,17 +23,17 @@ function checkType(value: StringOrNumber): string | number {
     return 'Number';
   }
 }
-const resultP3 = checkType('Hello');
-const resultP31 = checkType(42);
-console.log(resultP3, resultP31);
+const result3 = checkType('Hello');
+const result3a = checkType(42);
+console.log(result3, result3a);
 
 //=========>>>Problem 4<<<===========//
 function getProperty<T, X extends keyof T>(obj: T, key: X): T[X] {
   return obj[key];
 }
 const user = { id: 1, name: 'John Doe', age: 21 };
-const resultP4 = getProperty(user, 'name');
-console.log(resultP4);
+const result4 = getProperty(user, 'name');
+console.log(result4);
 
 //=========>>>Problem 5<<<===========//
 interface Book {
@@ -76,3 +76,9 @@ class Student extends Person {
 }
 const student = new Student('Alice', 20, 'A');
 console.log(student.getDetails());
+//=========>>>Problem 7<<<===========//
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((matchItem) => arr2.includes(matchItem));
+}
+const result7 = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+console.log(result7);
