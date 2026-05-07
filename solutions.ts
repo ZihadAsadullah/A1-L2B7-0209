40a257c216ae14d4682aacee293fd8669cb8cb32
Asadullah <maazihadin@gmail.com>
2026-05-07 18:27:38 +0600

    Problem 6 complete

--- solutions.ts
+++ solutions.ts
@@ -54,3 +54,25 @@ const myBook = {
 };
 const result5 = toggleReadStatus(myBook);
 console.log(result5);
+
+//=========>>>Problem 6<<<===========//
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+const student = new Student('Alice', 20, 'A');
+console.log(student.getDetails());
